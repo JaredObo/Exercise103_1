c4ee81620ff5de456bce31bd0a4f275c86aa2475
Jared Obozele <jared.obozele@gmail.com>
2019-12-06 15:52:03 -0500

    new files added

---
+++ src/main/java/com/example/exercise103_1/Exercise1031Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise103_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1031Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise1031Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercise103_1/Exercise1031ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise103_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1031ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
